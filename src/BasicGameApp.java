dbbf92060ca6e8fd847880bc7fb68133dbe0dd17
chloekocoloski <chloe_kocoloski27@milton.edu>
2025-05-16 13:49:45 -0400

    Homework in class

---
+++ src/BasicGameApp.java
@@ -0,0 +1,7 @@
+public class BasicGameApp {
+    public String number; //ex: Ace, 2, King
+    public String suit;
+    public boolean isEleven; //this is for Aces
+    public int value; //ex: 11, 2, 3, .... 10
+
+}
