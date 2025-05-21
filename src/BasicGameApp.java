92aa29851c02fd2c7a5f785470a14562eb18feb2
chloekocoloski <chloe_kocoloski27@milton.edu>
2025-05-21 12:29:42 -0400

    Homework : write variables

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -1,8 +1,30 @@
 public class BasicGameApp {
+  public static void main(String[] args) {
+    BasicGameApp go = new BasicGameApp();
+  }
   public Player player;
   public Dealer dealer;
-
   public Card[] deck;
 
 
+  public BasicGameApp() {
+    System.out.println("Let's play Blackjack!");
+
+    deck = new Card [52];
+
+    for(int all = 0; all < 4; all++) {
+
+      for (int x = 0; x < 13; x++) {
+        //object called: "FirstCard"
+        Card FirstCard = new Card(x + 1, all);
+        FirstCard.printMethod();
+
+        //put "FirstCard" into deck at index 0
+        deck[x] = FirstCard;
+      }
+
+    }
+
+
+  }
 }
--- src/Card.java
+++ src/Card.java
@@ -5,17 +5,25 @@ public class Card {
     public boolean isEleven; //for aces
     public int value; //ex: 11, 2, 7,
 
-    public Card(){
+    public Card(int pnumber, int psuit){
         number = "six";
-        suit = "Diamond";
-        value = 6;
+        value = pnumber;
         isEleven = false;
 
-        printMethod();
+        if(psuit == 2){
+            suit = "Hearts";
+        }
+
+        if(pnumber == 13){
+            number = "King";
+            value = 10;
+        }
+
+//        printMethod();
     }
 
     public void printMethod(){
-        System.out.println("Your card is a " + number + ". It has a " + suit + " suit. It has a value of " + value + ". It is " + isEleven + "that it is Eleven.");
+        System.out.println("Your card is a " + number + ". It has a " + suit + " suit. It has a value of " + value + ". It is " + isEleven + " that it is Eleven.");
     }
 
 }
